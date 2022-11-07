d1bf3bdbad24355bce619a0f7865326333e99165
3ryand <eryandhiputro@gmail.com>
2022-11-07 11:26:29 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,20 @@
 //navbarfix
 window.onscroll = function () {
   const header = document.querySelector("header");
+  const portof = document.querySelector("#portofolio");
   const fixedNav = header.offsetTop;
+  const fixedNav2 = portof.offsetTop;
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
   } else {
     header.classList.remove("navbar-fixed");
+  }
+
+  if (window.pageYOffset > fixedNav2) {
+    header.classList.add("navbar-fixed2");
+  } else {
+    header.classList.remove("navbar-fixed2");
   }
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,6 @@ module.exports = {
       padding: "16px",
     },
     extend: {
-      keyframes: {
-        wave: {
-          "0%": { transform: "rotate(0.0deg)" },
-          "10%": { transform: "rotate(14deg)" },
-          "20%": { transform: "rotate(-8deg)" },
-          "30%": { transform: "rotate(14deg)" },
-          "40%": { transform: "rotate(-4deg)" },
-          "50%": { transform: "rotate(10.0deg)" },
-          "60%": { transform: "rotate(0.0deg)" },
-          "100%": { transform: "rotate(0.0deg)" },
-        },
-      },
-      animation: {
-        "waving-hand": "wave 2s linear infinite",
-      },
       colors: {
         bgwhite: "#faf7f5",
         primary: "#65c3c8",
