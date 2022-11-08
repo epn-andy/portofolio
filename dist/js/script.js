3be6c8fec5d0e4a086ba0846d0dc706d151c0917
epn-andy <87350775+epn-andy@users.noreply.github.com>
2022-11-08 07:24:17 +0700

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,16 +2,18 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const portof = document.querySelector("#portofolio");
+  const batasPortof = document.querySelector("#contact");
   const fixedNav = header.offsetTop;
   const fixedNav2 = portof.offsetTop;
-
+  const fixBatas = batasPortof.OffsetTop;
+  
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
   } else {
     header.classList.remove("navbar-fixed");
   }
 
-  if (window.pageYOffset > fixedNav2) {
+  if (window.pageYOffset > fixedNav2 && window.pageYOffset < fixBatas) {
     header.classList.add("navbar-fixed2");
   } else {
     header.classList.remove("navbar-fixed2");
